6ee634b2548c0793a3f67dfcc1b6abbb3aa9ca64
Sebazzz <triadsebas@gmail.com>
2013-10-14 22:14:07 +0200

    Added unit testing stub

---
+++ src/SDammann.MyMvvm/SDammann.MyMvvm.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SDammann.MyMvvm.Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
